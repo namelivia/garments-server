ea5579c4fddf45665d8a5a43bd02ddc6d3fba078
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-01-17 22:11:33 +0100

    Tweak the image loader

--- scripts/image_loader.py
+++ scripts/image_loader.py
@@ -11,21 +11,38 @@ all_garments = crud.get_garments(db)
 for garment in all_garments:
     id = garment.id
     print(f"The id is :{id}")
-    print(f"Does {id}.jpg exist?")
+    print(f"Does {id}.png exist?")
     try:
-        f = open(f"images/{id}.jpg", "rb")
-        print("YES")
-        print("Uploading image")
-        response = requests.post(
-            url=os.getenv("IMAGES_SERVICE_ENDPOINT") + "/image",
-            files={"media": f.read()},
-        )
-        print(response.text)
-        location = response.headers["location"]
-        garment.image = location
-        db.commit()
-        print(location)
+        with open(f"images/{id}.png", "rb") as f:
+            print("YES")
+            print("Uploading image")
+            response = requests.post(
+                url=os.getenv("IMAGES_SERVICE_ENDPOINT") + "/image",
+                files={"media": f.read()},
+            )
+            print(response.text)
+            location = response.headers["location"]
+            garment.image = location
+            db.commit()
+            print(location)
     except IOError:
         print("It does not exist")
-    finally:
-        f.close()
+
+    name = garment.name
+    print(f"The name is :{name}")
+    print(f"Does {name}.png exist?")
+    try:
+        with open(f"images/{id}.png", "rb") as f:
+            print("YES")
+            print("Uploading image")
+            response = requests.post(
+                url=os.getenv("IMAGES_SERVICE_ENDPOINT") + "/image",
+                files={"media": f.read()},
+            )
+            print(response.text)
+            location = response.headers["location"]
+            garment.image = location
+            db.commit()
+            print(location)
+    except IOError:
+        print("It does not exist")
