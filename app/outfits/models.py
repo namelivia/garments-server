32e4c99ebc985803311dc00fe82b6b4795c0719d
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2024-03-08 12:36:41 +0100

    Add intermediate table

--- app/outfits/models.py
+++ app/outfits/models.py
@@ -1,8 +1,18 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+
+from sqlalchemy.orm import relationship
 from app.database import Base
 from app.garments.models import Garment
+
+outfit_garment = Table(
+    "outfit_garment",
+    Base.metadata,
+    Column("outfit_id", ForeignKey("outfits.id")),
+    Column("garment_id", ForeignKey("garments.id")),
+)
 
 
 class Outfit(Base):
     __tablename__ = "outfits"
     id = Column(Integer, primary_key=True, index=True)
+    garments = relationship(Garment, secondary=outfit_garment)
